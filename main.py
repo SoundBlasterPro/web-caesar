79c76c0fe87d7484acf4908011c80e1e03227cc7
William Reed <william_reed@live.com>
2017-06-09 23:08:50 -0700

    Program escapes user input, handles blank rotation, rotation by non-integer values

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ form = """
     <body>
         <form method="POST">
                 <label for="rot">Rotate By: </label>
-                <input name="rot" type="text" value="{0}" /><div class="error">{2}</div>
+                <input name="rot" type="text" value="{0}" id="rot" /><div class="error">{2}</div>
                 <textarea name="text" placeholder="Enter your text here...">{1}</textarea>
                 <input type="submit" />
         </form>
@@ -44,12 +44,24 @@ form = """
 
 @app.route("/", methods = ["POST"])
 def encrypt():
+
     text = request.form["text"]
     rot = request.form["rot"]
-    if not rot:
-        error = "Please enter a valid rotation value!"
-        return form.format(0,"",error) 
-    return form.format(rot,rotate_string(text, int(rot)),"") 
+
+    if not rot: #user submits a blank rotation field
+        error = "Please enter a rotation value!"
+        return form.format(0,text,error) 
+    if not is_integer(rot): #test if user enters a value that is not an integer
+        error = "Please enter a valid integer!"
+        return form.format(0,text,error)
+    return form.format(rot,rotate_string(cgi.escape(text), int(rot)),"") 
+
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
 
 @app.route("/")
 def index():
